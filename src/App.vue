0190fd9e379381bb0e8c48dd219ad27252c17775
JacobGlitcher <hakob.yer@gmail.com>
2022-11-14 18:38:50 +0400

    project init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Todos from '@components/Todos.vue'
+
+export default defineComponent({
+  data() {
+    return {
+    }
+  },
+  components: {
+    Todos
+  },
+  methods: {
+  },
+  computed: {
+  }
+})
+</script>
+
+<template>
+  <main>
+    <Todos/>
+  </main>
+</template>
+
+<style lang="scss">
+  @import "assets/styles/sass/main";
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    background-color: #eaffea;
+  }
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+    }
+  },
+})
+</script>
+
+<template>
+  <p>lorem</p>
+</template>
+
+<style lang="scss" scoped>
+</style>
